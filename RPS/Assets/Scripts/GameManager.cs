244c1506560728f989ad612fa5645765b11f1a49
Garrett6419 <garrettbequette6419@gmail.com>
2024-10-05 06:14:00 -0400

    Added Hearts

--- RPS/Assets/Scripts/GameManager.cs
+++ RPS/Assets/Scripts/GameManager.cs
@@ -12,13 +12,17 @@ public class GameManager : MonoBehaviour
     public List<Card> Hand = new List<Card>() ;
     public int battleHearts = 3; 
     public int gameHearts = 3 ;
+    public List<Card> battleHeartsList  = new List<Card>();
+    public List<Card> gameHeartsList = new List<Card>();
+
 
     // ENEMY VARIABLES
     public List<Card> enemyDeck = new List<Card>() ;
     public Transform[] enemyCardSlots;
-    public int  enemyHandSize;
+    public int enemyHandSize;
     public List<Card> enemyHand = new List<Card>() ;
     public int enemyHearts = 3;
+    public List<Card> enemyHeartsList = new List<Card>() ;
 
     public void Start() {
         getHand() ;
@@ -64,19 +68,28 @@ public class GameManager : MonoBehaviour
     }
 
     public void takeDamage() {
+        battleHeartsList[battleHearts-1].gameObject.SetActive(false) ;
         battleHearts-- ;
+        
         if(battleHearts == 0 ) {
+            gameHeartsList[gameHearts-1].gameObject.SetActive(false) ;
             gameHearts--; 
             if(gameHearts < 1) {
                 gameOver() ;
             }
             battleHearts = 3 ;
+            for(int i = 0 ; i < 3 ; i++) {
+                battleHeartsList[i].gameObject.SetActive(true) ;
+            }
+
 
         }
     }
 
     public void dealDamage() {
+        enemyHeartsList[enemyHearts-1].gameObject.SetActive(false) ;
         enemyHearts--;
+
         if(enemyHearts < 1) {
             resetBoard() ;
             playerWins() ;
@@ -84,12 +97,12 @@ public class GameManager : MonoBehaviour
     }
 
     public void resetBoard() {
-        for(int i = 0 ; i <  enemyHandSize ; i++) {
+        for(int i = enemyHandSize - 1 ; i >= 0 ; i--) {
             enemyHand[i].gameObject.SetActive(false) ;
             enemyDeck.Add(enemyHand[i]) ;
             enemyHand.Remove(enemyHand[i]) ;
         }
-        for(int i = 0 ; i <  Hand.Count ; i++) {
+        for(int i = Hand.Count -1 ; i >= 0 ; i--) {
             Hand[i].gameObject.SetActive(false) ;
             deck.Add(Hand[i]) ;
             Hand.Remove(Hand[i]) ;
