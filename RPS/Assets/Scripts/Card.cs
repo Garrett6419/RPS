6c4746cb08e174fab44c703e6f50d7093b16cb13
Garrett6419 <garrettbequette6419@gmail.com>
2024-10-05 05:28:56 -0400

    Updates to Card and GameManager

--- RPS/Assets/Scripts/Card.cs
+++ RPS/Assets/Scripts/Card.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public bool hasBeenPlayed ;
-    public int handIndex ;
+    public int cardType ;
+    private GameManager gm ;
+
+    private int[][] typeChart  = new int[3][3] ; { 0, -1, 1, 1, 0, -1, -1, 1, 0 } ;
+    private void Start() {
+        gm = FindObjectOfType<GameManager>() ;
+    }
 
     private void OnMouseDown() {
-        if(hasBeenPlayed == false){
+        battle(this) ;
+    }
 
-        }
+    public int getWinner(int enemyCardType) {
+        return typeChart[cardType][enemyCardType] ;
     }
 }
--- RPS/Assets/Scripts/GameManager.cs
+++ RPS/Assets/Scripts/GameManager.cs
@@ -10,12 +10,19 @@ public class GameManager : MonoBehaviour
     public Transform[] cardSlots;
     public int playerHandSize;
     public List<Card> Hand = new List<Card>() ;
+    public int battleHearts = 3; 
+    public int gameHearts = 3 ;
 
     // ENEMY VARIABLES
     public List<Card> enemyDeck = new List<Card>() ;
     public Transform[] enemyCardSlots;
     public int  enemyHandSize;
     public List<Card> enemyHand = new List<Card>() ;
+    public int enemyHearts = 3;
+
+    public void Start() {
+        getHand() ;
+    }
 
 
     // Function to draw a random card
@@ -39,5 +46,66 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
+    public void battle(Card playerCard) {
+        int enemyCardType = enemyHand[Random.Range(0, enemyHandSize)].cardType ;
+        int winner = playerCard.getWinner(enemyCardType) ;
+        if(winner == 0) {
+            resetBoard() ;
+        }
+        else if(winner == 1) {
+            dealDamage() ;
+            resetBoard() ;
+        }
+        else if(winner == -1) {
+            takeDamage() ;
+            resetBoard() ;
+        }
+    }
+
+    public void takeDamage() {
+        battleHearts-- ;
+        if(battleHearts == 0 ) {
+            gameHearts--; 
+            if(gameHearts < 1) {
+                gameOver() ;
+            }
+            battleHearts = 3 ;
+
+        }
+    }
+
+    public void dealDamage() {
+        enemyHearts--;
+        if(enemyHearts < 1) {
+            resetBoard() ;
+            playerWins() ;
+        }
+    }
+
+    public void resetBoard() {
+        for(int i = 0 ; i <  enemyHandSize ; i++) {
+            enemyHand[i].gameObject.SetActive(false) ;
+            enemyDeck.Add(enemyHand[i]) ;
+            enemyHand.Remove(enemyHand[i]) ;
+        }
+        for(int i = 0 ; i <  Hand.Count ; i++) {
+            Hand[i].gameObject.SetActive(false) ;
+            deck.Add(Hand[i]) ;
+            Hand.Remove(Hand[i]) ;
+        }
+        getHand() ;
+
+    }
+
+    public void playerWins() {
+        Debug.Log("Player wins!") ;
+    }
+
+    public void gameOver() {
+        Debug.Log("Game Over!") ;
+    }
+
+
 
 }
