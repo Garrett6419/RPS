bc2ad2a0287490e80a0be1d7f94430f0989bf575
Garrett6419 <garrettbequette6419@gmail.com>
2024-10-05 21:39:05 -0400

    Added sliding transitions for the cards

--- RPS/Assets/Scripts/GameManager.cs
+++ RPS/Assets/Scripts/GameManager.cs
@@ -3,229 +3,368 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-
 public class GameManager : MonoBehaviour
 {
     //Cams
     public Camera cam1;
     public Camera cam2;
 
-
     // PLAYER VARIABLES
     public List<Card> deck = new List<Card>();
-    public int deckSize ;
+    public int deckSize;
     public Transform[] cardSlots;
+    public Transform playerDeckTran;
     public int playerHandSize;
-    public List<Card> Hand = new List<Card>() ;
-    public int battleHearts = 3; 
-    public int gameHearts = 3 ;
-    public List<Card> battleHeartsList  = new List<Card>();
+    public List<Card> Hand = new List<Card>();
+    public int battleHearts = 3;
+    public int gameHearts = 3;
+    public List<Card> battleHeartsList = new List<Card>();
     public List<Card> gameHeartsList = new List<Card>();
 
-
     // ENEMY VARIABLES
-    public List<Card> enemyDeck = new List<Card>() ;
-    public int enemyDeckSize ;
+    public List<Card> enemyDeck = new List<Card>();
+    public int enemyDeckSize;
     public Transform[] enemyCardSlots;
+    public Transform enemyDeckTran;
     public int enemyHandSize;
-    public List<Card> enemyHand = new List<Card>() ;
+    public List<Card> enemyHand = new List<Card>();
     public int enemyHearts = 3;
-    public List<Card> enemyHeartsList = new List<Card>() ;
+    public List<Card> enemyHeartsList = new List<Card>();
 
     // GAME VARIABLES
-    public int gameRound ;
+    public int gameRound;
+    public bool isBossRound = false;
+    public int cardSpeed = 1;
 
     // Shop Variables
     public int Money = 10;
-    public Text moneyText ;
+    public Text moneyText;
     public Text ShopMoneyText;
-    public Text roundText ;
+    public Text roundText;
     public List<Card> shopCards = new List<Card>();
     public Transform[] shopCardSlots;
 
-
-    public void Start() {
+    public void Start()
+    {
         cam1.enabled = true;
         cam2.enabled = false;
-        getHand() ;
-        moneyText.text =  "Money: $" + Money ;
-        roundText.text = "Round: " + gameRound ;
-
+        StartCoroutine(GetHand());
+        moneyText.text = "Money: $" + Money;
+        roundText.text = "Round: " + gameRound;
     }
 
     // Function to draw a random card
-    public void getHand()
+    private IEnumerator GetHand()
     {
-        // Get The Player's Hand
-        for(int i = 0 ; i < playerHandSize ; i++) {
+        if (Hand == null || enemyHand == null)
+        {
+            yield break;
+        }
+        StartCoroutine(GetPlayerHand());
+        StartCoroutine(GetEnemyHand());
+        yield return new WaitUntil(() => Hand.Count == playerHandSize && enemyHand.Count == enemyHandSize);
+        // Add a small delay to ensure the cards are fully moved to their slots
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private IEnumerator GetPlayerHand()
+    {
+        for (int i = 0; i < playerHandSize; i++)
+        {
             Card randCard = deck[Random.Range(0, deck.Count)];
-            randCard.gameObject.SetActive(true) ;
-            randCard.transform.position = cardSlots[i].position ;
-            deck.Remove(randCard) ;
-            Hand.Add(randCard) ;
+            randCard.gameObject.SetActive(true);
+            randCard.transform.position = playerDeckTran.position;
+            yield return MoveCardToSlot(randCard, cardSlots[i].position, cardSpeed);
+            deck.Remove(randCard);
+            Hand.Add(randCard);
         }
-
-        for(int i = 0 ; i < enemyHandSize ; i++) {
-            Card randCard = enemyDeck[Random.Range(0, enemyDeckSize)] ;
-            randCard.gameObject.SetActive(true) ;
-            randCard.transform.position = enemyCardSlots[i].position ;
-            enemyDeck.Remove(randCard) ;
-            enemyDeckSize-- ;
-            enemyHand.Add(randCard) ;
-        }
-
     }
 
-    public void battle(Card playerCard) {
+    private IEnumerator GetEnemyHand()
+    {
+        for (int i = 0; i < enemyHandSize; i++)
+        {
+            Card randCard = enemyDeck[Random.Range(0, enemyDeckSize)];
+            randCard.gameObject.SetActive(true);
+            randCard.transform.position = enemyDeckTran.position;
+            yield return MoveCardToEnemySlot(randCard, enemyCardSlots[i].position, cardSpeed);
+            enemyDeck.Remove(randCard);
+            enemyDeckSize--;
+            enemyHand.Add(randCard);
+        }
+    }
+
+    private IEnumerator MoveCardToSlot(Card card, Vector2 destination, float speed)
+    {
+        while (Vector2.Distance(card.transform.position, destination) > 0.01f)
+        {
+            card.transform.position = Vector2.MoveTowards(card.transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        card.transform.position = destination;
+    }
+
+    private IEnumerator MoveCardToEnemySlot(Card card, Vector2 destination, float speed)
+    {
+        while (Vector2.Distance(card.transform.position, destination) > 0.01f)
+        {
+            card.transform.position = Vector2.MoveTowards(card.transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        card.transform.position = destination;
+    }
+
+    public void battle(Card playerCard)
+    {
         int enemyCardType = enemyPickCard();
-        int winner = playerCard.getWinner(enemyCardType) ;
-        if(winner == 0) {
-            resetBoard() ;
+        int winner = playerCard.getWinner(enemyCardType);
+        if (winner == 0)
+        {
+            resetBoard();
+            
         }
-        else if(winner == 2) {
-            int exp = Random.Range(1,2) ;
-            winner = 1 ;
-            for(int i = 0 ; i < exp ; i++) {
-                winner *= -1 ;
+        else if (winner == 2)
+        {
+            int exp = Random.Range(1, 2);
+            winner = 1;
+            for (int i = 0; i < exp; i++)
+            {
+                winner *= -1;
             }
         }
-        else if(winner == 1) {
-            dealDamage() ;
-            resetBoard() ;
+        if (winner == 1)
+        {
+            dealDamage();
+            resetBoard();
+            
         }
-        else if(winner == -1) {
-            takeDamage() ;
-            resetBoard() ;
+        else if (winner == -1)
+        {
+            takeDamage();
+            resetBoard();
+            
         }
     }
 
-    public void takeDamage() {
-        battleHeartsList[battleHearts-1].gameObject.SetActive(false) ;
-        battleHearts-- ;
-        
-        if(battleHearts == 0 ) {
-            gameHeartsList[gameHearts-1].gameObject.SetActive(false) ;
-            gameHearts--; 
-            if(gameHearts < 1) {
-                gameOver() ;
-            }
-            battleHearts = 3 ;
-            for(int i = 0 ; i < 3 ; i++) {
-                battleHeartsList[i].gameObject.SetActive(true) ;
-            }
+    public void takeDamage()
+    {
+        battleHeartsList[battleHearts - 1].gameObject.SetActive(false);
+        battleHearts--;
 
-
+        if (battleHearts == 0)
+        {
+            gameHeartsList[gameHearts - 1].gameObject.SetActive(false);
+            gameHearts--;
+            if (gameHearts < 1)
+            {
+                gameOver();
+            }
+            battleHearts = 3;
+            for (int i = 0; i <            3; i++)
+            {
+                battleHeartsList[i].gameObject.SetActive(true);
+            }
         }
     }
 
-    public void dealDamage() {
-        enemyHeartsList[enemyHearts-1].gameObject.SetActive(false) ;
+    public void dealDamage()
+    {
+        enemyHeartsList[enemyHearts - 1].gameObject.SetActive(false);
         enemyHearts--;
 
-        if(enemyHearts < 1) {
-            resetBoard() ;
-            increaseGameRound() ;
+        if (enemyHearts < 1)
+        {
+            resetBoard();
+            increaseGameRound();
+            
+        }
+        if (isBossRound && enemyHearts == 1)
+        {
+            refreshEnemy();
+            isBossRound = false;
         }
     }
 
-    public void resetBoard() {
-        for(int i = enemyHandSize - 1 ; i >= 0 ; i--) {
-            enemyHand[i].gameObject.SetActive(false) ;
-            enemyDeck.Insert(0, enemyHand[i]) ;
+    public void resetBoard()
+    {
+        StartCoroutine(ResetBoardCoroutine());
+    }
+
+    private IEnumerator ResetBoardCoroutine()
+    {
+        StartCoroutine(ResetPlayerHand());
+        StartCoroutine(ResetEnemyHand());
+        yield return new WaitUntil(() => Hand.Count == 0 && enemyHand.Count == 0);
+        yield return new WaitForEndOfFrame(); // wait for the frame to finish
+        OnResetBoardFinished(); // call the callback
+    }
+
+    private void OnResetBoardFinished()
+    {
+        // this method will be called when ResetBoardCoroutine finishes
+        StartCoroutine(GetHand());
+    }
+
+    private IEnumerator ResetEnemyHand()
+    {
+        for (int i = enemyHand.Count - 1; i >= 0; i--)
+        {
+            Card card = enemyHand[i];
+            yield return MoveCardToEnemyDeck(card, enemyDeckTran.position, cardSpeed);
+            card.gameObject.SetActive(false);
+            enemyDeck.Insert(0, card);
             enemyDeckSize++ ;
-            enemyHand.Remove(enemyHand[i]) ;
+            enemyHand.RemoveAt(i);
         }
-        for(int i = Hand.Count -1 ; i >= 0 ; i--) {
-            Hand[i].gameObject.SetActive(false) ;
-            deck.Add(Hand[i]) ;
-            Hand.Remove(Hand[i]) ;
-        }
-        getHand() ;
     }
 
-    public int enemyPickCard() {
-        int chosenCard = enemyHand[Random.Range(0, enemyHandSize)].cardType ;
-        int[] enemyCardUtility = new int[enemyHandSize] ;
-        int min = 100 ;
-        int max = -100 ;
-        for(int i = 0 ; i < enemyHandSize ; i++) {
-            enemyCardUtility[i] = 0 ;
-            for(int j = 0 ; j < playerHandSize ; j++) {
-                enemyCardUtility[i] -= Hand[j].getWinner(enemyHand[i].cardType) ;
+    private IEnumerator ResetPlayerHand()
+    {
+        for (int i = Hand.Count - 1; i >= 0; i--)
+        {
+            Card card = Hand[i];
+            yield return MoveCardToDeck(card, playerDeckTran.position, cardSpeed);
+            card.gameObject.SetActive(false);
+            deck.Add(card);
+            deckSize++;
+            Hand.RemoveAt(i);
+        }
+    }
+
+    private IEnumerator MoveCardToDeck(Card card, Vector2 destination, float speed)
+    {
+        while (Vector2.Distance(card.transform.position, destination) > 0.01f)
+        {
+            card.transform.position = Vector2.MoveTowards(card.transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        card.transform.position = destination;
+    }
+
+    private IEnumerator MoveCardToEnemyDeck(Card card, Vector2 destination, float speed)
+    {
+        while (Vector2.Distance(card.transform.position, destination) > 0.01f)
+        {
+            card.transform.position = Vector2.MoveTowards(card.transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        card.transform.position = destination;
+    }
+
+    public int enemyPickCard()
+    {
+        int chosenCard = enemyHand[Random.Range(0, enemyHandSize)].cardType;
+        int[] enemyCardUtility = new int[enemyHandSize];
+        int min = 100;
+        int max = -100;
+        for (int i = 0; i < enemyHandSize; i++)
+        {
+            enemyCardUtility[i] = 0;
+            for (int j = 0; j < playerHandSize; j++)
+            {
+                enemyCardUtility[i] -= Hand[j].getWinner(enemyHand[i].cardType);
             }
-            if(enemyCardUtility[i] < min) {
-                min = enemyHand[i].cardType ;
+            if (enemyCardUtility[i] < min)
+            {
+                min = enemyHand[i].cardType;
             }
-            if(enemyCardUtility[i] > max) {
-                max = enemyHand[i].cardType ;
+            if (enemyCardUtility[i] > max)
+            {
+                max = enemyHand[i].cardType;
             }
         }
 
-        if(gameRound < 3) {
+        if (gameRound < 3)
+        {
             return min;
         }
-        else if(gameRound < 6) {
-            return chosenCard ;
+        else if (gameRound < 6)
+        {
+            return chosenCard;
         }
-        else if(gameRound >= 6) {
-            return max ;
+        else if (gameRound >= 6)
+        {
+            return max;
         }
-        return 0 ;
+        return 0;
     }
 
-    public void playerWins() {
-        Debug.Log("Player wins!") ;
+    public void playerWins()
+    {
+        Debug.Log("Player wins!");
     }
 
-    public void gameOver() {
-        Debug.Log("Game Over!") ;
+    public void gameOver()
+    {
+        Debug.Log("Game Over!");
     }
 
-    public void increaseGameRound() {
-        gameRound++ ;
-        updateEnemyDeck() ;
-        refreshEnemy() ;
-        refreshPlayer() ;
-        if(gameRound >= 10) {
-            playerWins() ;
+    public void increaseGameRound()
+    {
+        gameRound++;
+        updateEnemyDeck();
+        refreshEnemy();
+        refreshPlayer();
+        if (gameRound >= 9)
+        {
+            isBossRound = true;
+        }
+        if (gameRound >= 10)
+        {
+            playerWins();
         }
         Money += 5;
-        changeScene();
-        initShop();
-
+        StartCoroutine(HideAllCards());
     }
 
-    public void refreshEnemy() {
-        enemyHearts = 3 ;
-        for(int i = 0 ; i < enemyHearts ; i++) {
-            enemyHeartsList[i].gameObject.SetActive(true) ;
+    public void refreshEnemy()
+    {
+        enemyHearts = 3;
+        for (int i = 0; i < enemyHearts; i++)
+        {
+            enemyHeartsList[i].gameObject.SetActive(true);
         }
     }
 
-    public void refreshPlayer() {
-        battleHearts = 3 ;
-        for(int i = 0 ; i < battleHearts ; i++) {
-            battleHeartsList[i].gameObject.SetActive(true) ;
+    public void refreshPlayer()
+    {
+        battleHearts = 3;
+        for (int i = 0; i < battleHearts; i++)
+        {
+            battleHeartsList[i].gameObject.SetActive(true);
         }
     }
 
-    public void updateEnemyDeck() {
-        for(int i = 0 ; i < 3 ; i++) {
-            int newCardPos = enemyDeckSize ;
-            int randCard =  Random.Range(newCardPos, enemyDeck.Count) ;
-            Card temp = enemyDeck[randCard] ;
-            enemyDeck.RemoveAt(randCard) ;
-            enemyDeck.Insert(newCardPos, temp) ;
-            enemyDeckSize++ ;
+    public void updateEnemyDeck()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int newCardPos = enemyDeckSize;
+            int randCard = Random.Range(newCardPos, enemyDeck.Count);
+            Card temp = enemyDeck[randCard];
+            enemyDeck.RemoveAt(randCard);
+            enemyDeck.Insert(0, temp);
+            enemyDeckSize++;
         }
     }
 
     public void changeScene()
     {
         cam1.enabled = !cam1.enabled;
-        cam2.enabled = !cam2.enabled;
+        cam2.enabled = !        cam2.enabled;
         moneyText.text = "Money: $" + Money;
-        roundText.text = "Round: " + gameRound;
+        if (isBossRound)
+        {
+            roundText.text = "Round: Boss Round!";
+        }
+        else
+        {
+            roundText.text = "Round: " + gameRound;
+        }
     }
 
     public void initShop()
@@ -241,5 +380,33 @@ public class GameManager : MonoBehaviour
         ShopMoneyText.text = "$" + Money;
     }
 
+    private IEnumerator HideAllCards()
+    {
+        foreach (Card card in deck)
+        {
+            card.gameObject.SetActive(false);
+        }
+        foreach (Card card in enemyDeck)
+        {
+            card.gameObject.SetActive(false);
+        }
+        foreach (Card card in Hand)
+        {
+            card.gameObject.SetActive(false);
+        }
+        foreach (Card card in enemyHand)
+        {
+            card.gameObject.SetActive(false);
+        }
+        yield return new WaitForEndOfFrame(); // wait for the frame to finish
+        OnAllCardsHidden(); // call the callback
+    }
+
+    private void OnAllCardsHidden()
+    {
+        // this method will be called when all cards are hidden
+        changeScene();
+        initShop();
+    }
 
 }
